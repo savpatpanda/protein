e6ffa39c5c99cfc6a1f268c047c8be24c6abf0ed
savpatpanda <savandrew1@gmail.com>
2020-10-18 01:28:49 -0400

    rotations

---
+++ proteinModel.py
@@ -0,0 +1,59 @@
+#imports
+import numpy as np
+from enum import Enum
+
+#inputs
+pdbFile = "4pcw.pdb"
+
+#constants
+class AtomType(Enum):
+	C = (5, "gray")
+	O = (5, "red")
+	N = (5, "blue")
+
+#functions
+def normalize(listOfAtoms):
+	firstResidue = listOfAtoms[0]
+	for i in range(len(listOfAtoms)):
+		listOfAtoms[i] = np.subtract(listOfAtoms[i],firstResidue)
+	return listOfAtoms
+
+def extractCoord(pdbFile, alpha = False):
+	pdb = open(pdbFile)
+	listOfAtoms = []
+
+	for line in pdb:
+		elements = line.split()
+		if elements[0] == "ATOM":
+			if alpha and elements[2] != "CA":
+				continue
+			coordinate = [float(elements[6]), float(elements[7]), float(elements[8])]
+			listOfAtoms.append(coordinate)
+
+	listOfAtoms = normalize(listOfAtoms)
+	return listOfAtoms
+
+def rotationMatrix(a, b, g):
+	zRot = np.matrix([[np.cos(a/180*np.pi), -1*np.sin(a/180*np.pi), 0],[np.sin(a/180*np.pi), np.cos(a/180*np.pi), 0],[0, 0, 1]])
+	yRot = np.matrix([[np.cos(b/180*np.pi), 0, np.sin(b/180*np.pi)],[0, 1, 0],[-1*np.sin(b/180*np.pi), 0, np.cos(b/180*np.pi)]])
+	xRot = np.matrix([[1, 0, 0],[0, np.cos(g/180*np.pi), -1*np.sin(g/180*np.pi)],[0, np.sin(g/180*np.pi), np.cos(g/180*np.pi)]])
+	endRot = np.matmul(np.matmul(zRot, yRot),xRot)
+	return endRot
+
+def rotate(coords, matrix):
+	setOfCoords = []
+	for i in range(len(coords)):
+		vector = [[coords[i][0]],[coords[i][1]],[coords[i][2]]]
+		output = matrix*vector
+		transpose = [0]*len(vector)
+		for i in range(len(transpose)):
+			transpose[i] = float(output[i][0])
+		setOfCoords.append(np.array(transpose))
+	return setOfCoords
+
+#init
+
+coords = extractCoord("4pcw.pdb", alpha = True)
+rotateMatrix = rotationMatrix(30, 0, 0)
+resulting = rotate(coords,rotateMatrix)
+print(resulting)
